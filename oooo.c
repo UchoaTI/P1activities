eadcfb75e0e02b101c0a1b32a476efbed06ca448
lucastroi <43651116+lucastroi@users.noreply.github.com>
2019-09-12 20:10:06 -0300

    Update oooo.c

--- oooo.c
+++ oooo.c
@@ -1,40 +1,42 @@
 #include <stdio.h> 
 #include <stdlib.h>
-#include <locale.h>
+#include <locale.h> 
 #include <math.h>
-
+#include <conio.h>
  
- int main(void){
- 	
+ int main(void){               //Questao 1
+ 	//AdaptaÃ§Ã£o do programa para PT-BR
  	
 	 setlocale(LC_ALL, "Portuguese");
  	
-	 //Definição de variavel
+	 //DefiniÃ§Ã£o de variavel
  	
-	 int a , num, in, c; 
-	 float cd1, cd2, cd3, cr, media, b, n1 = 1, n2 = 2;
+	 int a, num, in, c, x, y, k, pot; 
+	 float cd1, cd2, cd3, cr, media, b, n1 = 1, n2 = 2, mp;
 	 long decimal, q, r;
 	 int i, j = 0;
 	 char hexadecimal[100];
 	 char nome[45];
-	 int x, y, pot=0,k=0, count=0;
- 	 double w = 9.75;
-	 double h = 9.75;
+	 double w = 10;
+	 double h = 10;
 	 double logaritmo_natural = 0;
 	 double logaritmo_xbase10 = 0;
-	 //Informações sobre selecionaveis + leitor de variavel
  	
+	 //InformaÃ§Ãµes sobre selecionaveis + leitor de variavel
+ 	 system("cls");
 	 printf("\nDigite 0 para finalizar o programa\n"
-	       "1 Para o menu um\n"
-		   "2 Para o menu dois\n"
-		   "3 Para o menu três\n"
-		   "4 Para o menu quatro\n"
-		   "5 Para o menu cinco\n"
-		   "6 Para o menu seis\n"
-		   "7 Para o menu sete\n"
-		   "8 Para o menu oito\n"
-		   "9 Para o menu nove\n");
-	scanf("%d", &a);
+	       "1 para o menu um\n"
+		   "2 para o menu dois\n"
+		   "3 para o menu trÃªs\n"
+		   "4 para o menu quatro\n"
+		   "5 para o menu cinco\n"
+		   "6 para o menu seis\n"
+		   "7 para o menu sete\n"
+		   "8 para o menu oito\n"
+		   "9 para o menu nove\n"
+		   "10 para o menu dez\n");
+	scanf("%d", &a); 
+	system("cls");
 	
 	   //Estrutura a ser repitida
 	
@@ -42,22 +44,20 @@
 	{
 	   //Condicionais
 	   
-	   if(a == 1){
-	   	//questão 2 - informa se é par ou impar
-	   printf("Bem-vindo ao menu 1\n");
-	   printf("Digite um numero inteiro: ");
+	   if(a == 1){                            //Questao 2
+	   printf("Bem-vindo ao menu 1\n");  
+	   printf("digite um numero inteiro: ");     //Indentifica se o numero Ã© par ou impar
 		scanf("%d",&num);
 		if(num%2==0)//operador %
 		{
 		printf("Par");
 		}
 		else{
-		printf("Ímpar");
+		printf("Ãmpar");
 		}
        }
-	   else if(a == 2){
-	   	//questão 3 - primeiro numero potenciado ao segundo
-	   printf("Bem-vindo ao menu 2\n");
+	   else if(a == 2){                       //Questao 3
+	   printf("Bem-vindo ao menu 2\n");         //PotenciaÃ§Ã£o
 	   printf ("Primeiro numero: ");
      	scanf ("%d",&x);
      	printf ("Segundo numero: ");
@@ -65,37 +65,35 @@
  
      	for (k>0;k<y;k++)
      	{
-     	pot = x*(x*k); 
+     	pot = x*(x*k);
      	}
     	 
-        printf ("\n O resuldo é : %d", pot);
-       }
-	   
-	   else if(a == 3){
-	   	//questão 4 - logaritimos
-	   printf("Bem-vindo ao menu 3\n");
-	   printf("Digite o A e B para a operação logaritmo: \n");
+        printf ("\n O resuldo Ã© : %d", pot);
+	   }
+	   else if(a == 3){               
+	   printf("Bem-vindo ao menu 3\n");      //Logaritmo     //Questao 4
+	   printf("Digite suas 2 numeros para operaÃ§Ã£o logaritmo: \n");
 	   scanf("%f %f", &w, &h);  
 	   logaritmo_natural = log(w);
-	   w = 2.718282;
-	   printf("Logaritmo natural de A %.2f = %.2f\n",w, logaritmo_natural);
-	   w = 10;
+	   
+	   printf("Logaritmo natural de x %.2f = %.2f\n",w, logaritmo_natural);
+	
 	   logaritmo_xbase10 = log10(w);
-	   printf("Logaritmo de B na base10 %.2f = %.2f\n",w, logaritmo_xbase10);
+	   printf("Logaritmo de x na base10 %.2f = %.2f\n",w, logaritmo_xbase10);
 	   
 	   logaritmo_natural = log(h);
-	   h = 2.718282;
-	   printf("Logaritmo natural de A %.2f = %.2f\n",w, logaritmo_natural);
-	   h = 10;
+	   
+	   printf("Logaritmo natural de y %.2f = %.2f\n",w, logaritmo_natural);
+	   
 	   logaritmo_xbase10 = log10(h);
-	   printf("Logaritmo de B na base10 %.2f = %.2f\n",w, logaritmo_xbase10);
+	   printf("Logaritmo de y na base10 %.2f = %.2f\n",w, logaritmo_xbase10);
+	   
        }
-	   else if(a == 4){
-	   	//questão 5 - informa se o ano é ou não bissexto
-	   printf("Bem-vindo ao menu 4\n");
-	  printf("Digite um ano: ");
+	   else if(a == 4){                   //Questao 5
+	   printf("Bem-vindo ao menu 4\n");   //Indentifica se o ano Ã© bissexto ou nao
+	  printf("digite um ano: ");
 		scanf("%d",&num);
-		if(num%4==0)//operador %
+		if(num%4==0)   //operador %
 		{
 		printf("Ano Bissexto");
 		}
@@ -103,37 +101,38 @@
 		printf("Ano nao Bissexto");
 		}
        }
-	   else if(a == 5){
-	   	//questão 6 - media e cr
-	   printf("Bem-vindo ao menu 5\n");
+	   else if(a == 5){                    //Questao 6
+	   printf("Bem-vindo ao menu 5\n");       
 	  printf("Digite suas 3 notas, para o calculo da media\n");
 	   scanf("%f %f %f", &cd1, &cd2, &cd3);
-	   cr=(cd1*n1)+(cd2+n1)+(cd3+n2)/cd1+cd2+cd3;
-	   media = (cd1 + cd2 + cd3)/3;
-	   printf("Sua media é %f\n", media);
-	   printf("Seu CRE é %f\n", cr);
+	   
+	         cr=(cd1*n1)+(cd2*n1)+(cd3*n2)/cd1+cd2+cd3; //Calculo do CRE
+	         media = (cd1 + cd2 + cd3)/3;               //Calculo da media
+	         mp=(cd1*n1)+(cd2*n1)+(cd3*n2)/1+2+3;             //Calculo da media ponderada
+	   
+	   printf("Sua media Ã© %f\n", media);
+	   printf("Media pondera Ã© %f\n", mp);
+	   printf("Seu CRE Ã© %f\n", cr);
 	   if(media>=6.0){
 	   	printf("Parabens, voce foi aprovado!");
 	   }
 	   else
 	   printf("Infelizmente, voce foi reprovado!");
        }
-	   else if(a == 6){
-	   	//questão 7 - media de notas , ou nota invalida
+	   else if(a == 6){                      //Questao 7
 	   printf("Bem-vindo ao menu 6\n");
-	    printf("Digite suas 2 notas, para o calculo da media\n");
-	   scanf("%f %f", &cd1, &cd2);
+	     printf("Digite suas 2 notas, para o calculo da media\n"); //Calculo da media
+	   scanf("%f %f", &cd1, &cd2);  
 	   media = (cd1 + cd2)/2;
 	   
-	   if(cd1>0 && cd1<=10 && cd2>0 && cd2<=10){
-	   	printf("Sua media é %f", media);
+	   if(cd1>0 && cd1<=10 && cd2>0 && cd2<=10){         //Notas apenas entre 0 e 10
+	   	printf("Sua media Ã© %f", media);
        }
        else{
-       	printf("ERROR404");
+       	printf("ERROR404\n");
 	   } 
         
        }
-	
 	   else if(a == 7){
 	   printf("Bem-vindo ao menu 7\n");
   	   }
@@ -141,11 +140,13 @@
 	   printf("Bem-vindo ao menu 8\n");
 	   }
 	   else if(a == 9){
-	   	//questão 8 - matricula em hexadecimal
 	   printf("Bem-vindo ao menu 9\n");
-       printf("Disponha sua matricula: \n");
+	   }
+	   else if(a == 10){                    //Questao 8
+	   printf("Bem-vindo ao menu 10\n"); //TransformaÃ§Ã£o da matricula em hexa-decimal e a repitaÃ§Ã£o do nome
+       printf("sua matricula, por favor: \n");
 	   scanf("%ld", &decimal);
-	   printf("Disponha seu nome: ");
+	   printf("seu nome, por favor: ");
 	   scanf("%s",nome);
 	 
 	   q = decimal;
@@ -159,8 +160,8 @@
 	   hexadecimal[j++] = 55 + r;   
 	   q = q / 16;            
 	   }
-	   printf("Olá %s ",nome);
-	   printf("Sua matricula em hexadecimal :",decimal);
+	   printf("olÃ¡ %s ",nome);
+	   printf("sua matricula em hexadecimal :",decimal);
 	   
 	 
 	 
@@ -169,25 +170,30 @@
 	  printf("\n");
 	 }
 	   else{ 
-	   printf("\nComando inválido, por favor digite outro número: ");
+	   printf("\nComando invÃ¡lido, por favor digite outro nÃºmero: ");
        } 
-    //Opções a serem repetidas + Leitor de variavel
-
+    //OpÃ§Ãµes a serem repetidas + Leitor de variavel
+    
 	printf("\nDigite 0 para finalizar o programa\n"
 	       "1 para o menu um\n"
 		   "2 para o menu dois\n"
-		   "3 para o menu três\n"
+		   "3 para o menu trÃªs\n"
 		   "4 para o menu quatro\n"
 		   "5 para o menu cinco\n"
 		   "6 para o menu seis\n"
 		   "7 para o menu sete\n"
 		   "8 para o menu oito\n"
-		   "9 para o menu nove\n");
-	scanf("%d", &a);
+		   "9 para o menu nove\n"
+		   "10 para o menu dez\n");
+	scanf("%d", &a); 
+	system("cls");
+	
 	   
-    } 
-	  if (a==0)  //Finaliza o programa
-	printf("Programa finalizado!");
 
+	  if (a==0){  //Finaliza o programa
+	printf("Programa finalizado!");
+    }
+}
+    system("cls");
  	return 0;
  }
